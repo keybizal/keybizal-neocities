23de52423fbf3ddc5da0d67fee72f802c51af90a
keybizal <135049661+keybizal@users.noreply.github.com>
2026-04-18 17:14:32 -0500

    toggle button saves in storage, blog ui update, removed ability to read blogs on mobile instead of fixing it

--- public/global.js
+++ public/global.js
@@ -9,4 +9,21 @@ function toggleBackground() {
         document.body.style.backgroundImage = savedImage;
         isToggle = false;
     }
+    localStorage.setItem("toggle", isToggle);
 }
+
+function checkToggleState() {
+    const currentToggle = localStorage.getItem("toggle");
+    if (currentToggle == "true") {
+        toggleBackground();
+        //alert("success");
+    } else {
+        //alert("fail");
+    }
+
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    checkToggleState();
+}, false);
+    
