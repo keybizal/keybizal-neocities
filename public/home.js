835be9d17f260428a33b28463fcce7964c156bb5
keybizal <135049661+keybizal@users.noreply.github.com>
2026-09-24 00:05:33 -0500

    fixed bg (finally), added github repo link

--- public/home.js
+++ public/home.js
@@ -1,7 +1,7 @@
 
 function toTakeTheDead() {
     document.body.style.backgroundImage = "url(\"data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' version='1.1' height='50px' width='210px'><text x='0' y='15' fill='red' font-size='20' font-family='Courier New'>TO TAKE THE DEAD</text></svg>\")";
-    document.body.style.animation = "loopingText 5s linear infinite";
+    document.body.style.animation = "loopingDead 1s linear infinite";
     document.getElementById('theLogo').src = "SPEC_CLAY.gif";
     document.getElementById('t').style.border = "groove red";
     document.getElementsByClassName('homeButton')[0].style.border = "groove red";
