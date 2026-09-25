7e7fe83b288f28d5c1ff3089329b8c6b32c15df2
keybizal <135049661+keybizal@users.noreply.github.com>
2026-09-25 13:24:49 -0500

    blog now works smoothly on mobile, just need to once again cleanup css

--- public/global.js
+++ public/global.js
@@ -26,4 +26,43 @@ function checkToggleState() {
 document.addEventListener('DOMContentLoaded', function() {
     checkToggleState();
 }, false);
-    
+
+
+
+
+
+
+
+
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openBlog();
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function openBlog() {
+    document.getElementById("blogUI").style.display = "block";
+    document.getElementById("exitButton").style.display = "block";
+}
+
+function closeBlog() {
+    document.getElementById("blogUI").style.display = "none";
+    document.getElementById("exitButton").style.display = "none";
+}
